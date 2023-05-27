a36e689cb905741e0fdc391de4934a99d8fe9a28
fbesoainp <chachibesoain1@gmail.com>
2023-05-26 20:03:42 -0400

    🔒️ Secure routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProfileView from '../views/ProfileView.vue'
+import store from '@/store'
 
 const routes = [
   {
@@ -7,11 +9,27 @@ const routes = [
     name: 'home',
     component: HomeView
   },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: ProfileView,
+    meta: { requiresAuth: true }
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    if (!store.state.user) {
+      next({ name: 'home' })
+      return
+    }
+  }
+  next()
 })
 
 export default router
